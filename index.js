bbf474842c185030ec7732fc4e7d01f829507d3e
e <2247594324@qq.com>
2021-02-23 15:42:37 +0800

    更新

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 let server = express()
 server.use(express.static(path.join(__dirname,'public')))
-server.listen(3000, () => {
+server.listen(80, () => {
     console.log('启动成功')
 })
 
