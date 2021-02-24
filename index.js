a9e9ac88bae8dfe54db29bbfbe30e53b08fd60ad
e <2247594324@qq.com>
2021-02-24 09:44:42 +0800

    更新

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 let server = express()
-server.use(express.static(path.join(__dirname,'public')))
+server.use(express.static(path.join(__dirname,'edu')))
 server.listen(3001, () => {
     console.log('启动成功')
 })
