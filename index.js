ef4d2a0736cbfb276411a7be817ed9a7939a4e68
e <2247594324@qq.com>
2021-02-24 10:21:52 +0800

    更新

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const path = require('path')
 let server = express()
-let userRouter = express.Router()
+
 server.use(express.static(path.join(__dirname,'edu')))
 server.listen(3001, () => {
     console.log('启动成功')
 })
 
-server.use('/user',userRouter)
+// let userRouter = express.Router()
+// server.use('/user',userRouter)
 
-userRouter.get('/', (req,res) => {
-    res.send('用户')
-})
+// userRouter.get('/', (req,res) => {
+//     res.send('用户')
+// })
 
 
-userRouter.get('/:id', (req,res) => {
-    res.send('动态路由')
-})
+// userRouter.get('/:id', (req,res) => {
+//     res.send('动态路由')
+// })
